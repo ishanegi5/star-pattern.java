83cc5e04823878e23a3975c6d451417f9d05bdf7
Isha Negi <itsishanegi@gmail.com>
2024-09-22 02:25:24 +0530

    Add files via upload

---
+++ star-pattern.java
@@ -0,0 +1,28 @@
+public class Main{
+    
+public static void main(String[]args){
+ 
+int i = 5;
+       
+while(i>0){
+           
+           
+int j=i;
+           
+while(j>0){
+               
+j--;
+               
+System.out.print("*");
+           
+}
+           
+i--;
+           
+System.out.println();
+       
+}
+ 
+}
+
+} 
